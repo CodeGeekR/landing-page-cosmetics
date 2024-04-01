fbd9dd8bc4bd5d95ee35e903c17e1eb835a42c40
macOS <macos@1EkiLop0.local>
2024-04-01 10:16:06 -0500

    add manager error

--- api/server.js
+++ api/server.js
@@ -19,22 +19,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json()); // Añade esto para analizar el cuerpo de las solicitudes JSON
 app.use(express.static(path.join(__dirname, '/')));
 
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY || '' });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/', 'index.html'));
-// });
-
-// mostrar un mensaje para las peticiones get
-app.get('/api/send', (req, res) => {
-    res.send('Esta es la App de Kaficosmetics');
-});
-
-
-app.post('/send', (req, res) => {
+app.post('/send', (req, res, next) => {
     const data = {
         from: 'Your Name <postmaster@mail.colombianmacstore.com.co>',
         to: 'sammydn7@gmail.com',
@@ -44,10 +35,13 @@ app.post('/send', (req, res) => {
 
     mg.messages.create('mail.colombianmacstore.com.co', data)
         .then(msg => res.json({ msg }))
-        .catch(err => {
-            console.error(err); // Agrega esta línea para registrar el error
-            res.status(500).json({ err });
-        });
+        .catch(err => next(err)); // Pasa el error al middleware de manejo de errores
+});
+
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Registra el stack trace del error
+    res.status(500).send('Something broke!');
 });
 
 module.exports = app; // Exporta la aplicación Express
