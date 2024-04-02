40247d3af347a7755a16edc35c78c3bc28618c57
macOS <macos@1EkiLop0.local>
2024-04-02 09:27:23 -0500

    modified send

--- api/send.js
+++ api/send.js
@@ -6,6 +6,8 @@ const mailgun = new Mailgun(formData);
 const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY || '' });
 
 module.exports = (req, res) => {
+    console.log('Función sin servidor invocada'); // Esta línea aparecerá en los registros de Vercel
+
     const data = {
         from: process.env.MAILGUN_FROM,
         to: process.env.MAILGUN_TO,
@@ -13,10 +15,15 @@ module.exports = (req, res) => {
         text: `Mensaje de: ${req.body.name} (${req.body.email})\n\n${req.body.message}`
     };
 
+    console.log('Enviando correo ...'); // Esta línea también aparecerá en los registros
+
     mg.messages.create(process.env.MAILGUN_DOMAIN, data)
-        .then(msg => res.json({ msg }))
+        .then(msg => {
+            console.log('Correo enviado con éxito!'); // Esta línea aparecerá en los registros si el correo se envía con éxito
+            res.json({ msg });
+        })
         .catch(err => {
-            console.error(err);
+            console.error('Error al enviar correo:', err); // Esta línea aparecerá en los registros si hay un error
             res.status(500).send(`¡Algo se rompió! Error: ${err.message}`);
         });
 };
